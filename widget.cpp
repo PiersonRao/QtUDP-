aea80790d1984f02da4ccdc2b0daddd60d355fe0
PiersonRao <54518619+PiersonRao@users.noreply.github.com>
2021-02-19 17:18:50 +0800

    Add files via upload

---
+++ widget.cpp
@@ -0,0 +1,52 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    m_pUdpSocket = new QUdpSocket(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_btn_Listen_clicked()
+{
+//    m_pUdpSocket->waitForDisconnected()
+    strIP = ui->ldt_IP->text();
+    nPort = ui->ldt_listen->text().toUInt();
+    bool bbind = m_pUdpSocket->bind(QHostAddress::AnyIPv4, nPort,QUdpSocket::ShareAddress);
+    bool bret = m_pUdpSocket->joinMulticastGroup(QHostAddress(strIP));
+
+    if(bbind && bret)
+    {
+        ui->textBrowser->append("UDP bind or join ok");
+    }
+    else
+    {
+        ui->textBrowser->append("UDP bind or join Error");
+    }
+    connect(m_pUdpSocket, &QUdpSocket::readyRead, this, &Widget::ReceiveUDP);
+}
+
+void Widget::on_btn_clear_clicked()
+{
+    ui->textBrowser->clear();
+}
+
+void Widget::ReceiveUDP()
+{
+    QByteArray byRecv;
+    if(m_pUdpSocket->hasPendingDatagrams())
+    {
+
+        byRecv.resize(m_pUdpSocket->pendingDatagramSize());
+        m_pUdpSocket->readDatagram(byRecv.data(), byRecv.size());
+        ui->textBrowser->append("收到数据："+byRecv.toHex().toUpper());
+    }
+}
---
+++ widget.h
@@ -0,0 +1,34 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QUdpSocket>
+#include <QHostAddress>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+private slots:
+    void on_btn_Listen_clicked();
+
+    void on_btn_clear_clicked();
+
+    void ReceiveUDP();
+
+private:
+    Ui::Widget *ui;
+    QUdpSocket *m_pUdpSocket;
+    QString strIP;
+    quint16 nPort;
+};
+
+#endif // WIDGET_H
